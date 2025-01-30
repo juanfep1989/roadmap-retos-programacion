7728ed0ca652aee31dc13879020ebf412aee22a1
juanfep1989 <94489424+juanfep1989@users.noreply.github.com>
2025-01-30 22:04:50 +0100

    juanfep1989.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/juanfep1989.js
@@ -0,0 +1,17 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+/*Este es un 
+comentario 
+en diferentes lineas */
+
+var nombre = "juan"; //declaracion usando la palabra reservada var
+let segundonombre = "felipe"; // declaracion de una variable con let y a su vez un string
+const apellido = "perdomo"; // declaracion  de una variable constante.
+const telefono = 658654651; // declaracin de un variable numerica
+const boleano = true; //una variable bollean puede tomar tambien el valor de false
+const nula = null; // una variable con valor null
+const flotante = 12.5 // variable del tipo float
+const ind // variable indefinida
+
+//imprimiendo por consola el mensaje requerido en el reto del dia de hoy
+const nombreLenguaje = "JavaScript";
+console.log("!Hola, "+nombreLenguaje);
